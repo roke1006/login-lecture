bf72d6da1feeccb3af9028c50d3a0800a8672fa6
roke1006 <roke1009@naver.com>
2022-08-28 20:32:15 +0900

    자바 스크리브 추가

--- app/app.js
+++ app/app.js
@@ -4,13 +4,13 @@
 const express = require("express");
 const app = express();
 
-
 //라우팅
 const home = require("./src/routes/home");
 
 //앱 세팅 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/src/public"));
 
 app.use("/", home); // use -> 미들 웨어를 등록 해주는 메서드
 
